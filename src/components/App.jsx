24fe1f460af59143b738ca8d2c0063805bad8334
Joshua Rose <joshuarose@outlook.com>
2019-05-22 17:41:29 -0400

    This works with router now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './About';
+import Story from './Story';
 
-const App = () => <div>Replace me with something you can be proud of.</div>;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/story/">Story</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Story} />
+          <Route path="/about/" component={About} />
+          <Route path="/story/" component={Story} />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Story.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Story extends Component {
+  render() {
+    return <div>This is the story of a boy.</div>;
+  }
+}
+
+export default Story;
